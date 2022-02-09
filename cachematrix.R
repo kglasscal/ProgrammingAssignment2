4f8ffba4fcc0824dece7db01dc448b8acd57e084
Kevin Glass <kevinglasscal@gmail.com>
2022-02-09 00:35:37 -0800

    Added and tested first revision of makeCacheMatrix and cacheSolve. Tested on a 4x4 matrix 2*I matrix. Need to add tests for non-square matrix error, and non-invertable error.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(A = matrix()) {
+  Ainv <- NULL
+  set <- function(B) {
+    A <<- B
+    Ainv <<- NULL
+  }
+  get <- function() A
+  setinverse <- function(B) Ainv <<- B
+  getinverse <- function() Ainv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(A, ...) {
         ## Return a matrix that is the inverse of 'x'
+  Ainv <- A$getinverse()
+  if(!is.null(Ainv)) {
+    message("getting cached data")
+    return(Ainv)
+  }
+  data <- A$get()
+  Ainv <- solve(data, ...)
+  A$setinverse(Ainv)
+  Ainv 
 }
